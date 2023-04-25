bc55a2c8e31ee01117aa7d859f68dcdf86e83f55
Giovani Tzec <giovanni.tzec@gmail.com>
2023-04-25 14:37:13 -0600

    UserSeeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,38 +20,12 @@ class UserSeeder extends Seeder
         $roleUsuario = Role::findOrFail(2);
         $roleEditor = Role::findOrFail(3);
 
-        $admin = User::create([
+        $user = User::create([
             'id' => 1,
-            'name' => 'Leonel',
-            'last_name' => 'Lopez',
-            // 'dui' => '12345678-1',
-            'email' => 'leonellopez647@gmail.com',
-            'password' => Hash::make('Leonel23'),
-            'name' => 'Leonel',
-            'email_verified_at' => now(),
-        ]);
-        $admin->assignRole($roleEditor);
-
-        $user = User::create([
-            'id' => 2,
-            'name' => 'Leonel',
-            'last_name' => 'Lopez',
-            // 'dui' => '12345678-2',
-            'email' => 'lopezleonel191@gmail.com',
-            'password' => Hash::make('Leonel23'),
-            'name' => 'Leonel',
-            'email_verified_at' => now(),
-        ]);
-        $user->assignRole($roleUsuario);
-
-        $user = User::create([
-            'id' => 3,
-            'name' => 'admin',
+            'name' => 'Administrador',
             'last_name' => 'admin',
-            // 'dui' => '02475605-7',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('12345678'),
-            'name' => 'William',
             'email_verified_at' => now(),
         ]);
         $user->assignRole($roleAdmin);
